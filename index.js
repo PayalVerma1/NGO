bdcc4e9d25ab4314148d39a72766398f4b8aba95
PayalVerma1 <payalvermaseema@gmail.com>
2025-01-08 17:02:15 +0530

    Hamburger added

---
+++ index.js
@@ -0,0 +1,6 @@
+
+  document.querySelector('.menu-icon').addEventListener('click', function () {
+    this.classList.toggle('clicked');
+    document.querySelector('.hamburger-menu').classList.toggle('open');
+  });
+
